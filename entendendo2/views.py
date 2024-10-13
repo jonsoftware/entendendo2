2d1a5b02de78d78234b3bc453c4963f7a43a3206
jonsoftware <jonas.ananias21@gmail.com>
2024-10-13 00:33:37 -0300

    entendendo 00:33

--- entendendo2/views.py
+++ entendendo2/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import entendendo2
 # Create your views here.
 
+
 def home(request):
-    return HttpResponse('Olá mundo');
+    return render(request, "entendendo2/home.html")
+
+def home(request):
+    return render(request, "entendendo2/entendendo2_list.html")
+
+def entendendo2_list(request):
+    nome = 'Rodrigo'  # Indentado corretamente
+    alunos = ['1. Ana', '2. José', '3. Bia']  # Indentado corretamente
+    return render(request, "entendendo2/entendendo2_list.html", {"nome": nome, "lista_alunos": alunos})  # Corrigida a indentação
+
+# Primeira função
+def entendendo2_list_old(request):
+    nome = 'Rodrigo'
+    alunos = ['1. Ana', '2. José', '3. Bia']
+    return render(request, "entendendo2/entendendo2_list.html", {"nome": nome, "lista_alunos": alunos})
+
+# Segunda função
+def entendendo2_list(request):
+    entendendo2_list = entendendo2.objects.all()
+    return render(request, "entendendo2/entendendo2_list.html", {"entendendendo2": entendendo2_list})
--- setup/urls.py
+++ setup/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from entendendo2.views import home
+from entendendo2.views import entendendo2_list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", home),
+    path("admin/", admin.site.urls),
+    path("", entendendo2_list),
 ]
+
+from entendendo2.views import entendendo2_list
+urlpatterns = [path("admin/", admin.site.urls), path("", entendendo2_list)]
